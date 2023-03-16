37eb771871e068416e59b09523122b13275cce2c
max <pmurariam@gmail.com>
2023-03-16 14:55:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pont
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	pringf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
