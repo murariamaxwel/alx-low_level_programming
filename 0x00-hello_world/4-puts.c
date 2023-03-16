cdf3441168bd186c6b0f2f68ebd050e408f5f06f
max <pmurariam@gmail.com>
2023-03-16 14:38:43 -0700

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ *main is the entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
